15af58c194b11f2eb11afd4c447ecda38692df1d
P0C0POCO <ksm991113@gmail.com>
2023-09-26 14:43:21 +0900

    player demo

---
+++ Software/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Move Property")]
+    [SerializeField] float playerSpeed = 5;
+    Rigidbody2D playerRigidbody;
+
+    [Header("Gun Property")]
+    [SerializeField] GameObject muzzle;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] GameObject gunPivot;
+    [SerializeField] Vector2 muzzleDirection;
+    Vector2 target;
+    Vector2 mouse;
+    float angle;
+
+    [Header("Slide Property")]
+    Vector2 slideDirection;
+    float slideSpeed = 5f;
+    float slideDuration = 0.5f;
+    float slideCooldown = 0.5f;
+    float slideTimer = 0f;
+    bool isSlide = false;
+
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        FollowMouse();
+        PlayerMove();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shooting();
+        }
+    }
+
+    public void PlayerMove()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector2 playerVelocity = new Vector2(inputX, inputY);
+        playerVelocity.Normalize();
+
+        playerRigidbody.velocity = playerVelocity * playerSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isSlide)
+        {
+            if (Mathf.Abs(inputX) > 0.1f || Mathf.Abs(inputY) > 0.1f)
+            {
+                slideDirection = playerVelocity.normalized;
+
+                StartCoroutine(Slide());
+            }
+        }
+    }
+
+    void FollowMouse() //CrossHair
+    {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
+        gunPivot.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void Shooting()
+    {
+        muzzleDirection = muzzle.transform.right;
+
+        GameObject _bullet = BulletPooler.Instance.GetBullet(BulletOwner.PLAYER);
+        _bullet.transform.position = muzzle.transform.position;
+        _bullet.GetComponent<Bullet>().SetDirection(muzzleDirection);
+        _bullet.SetActive(true);
+    }
+
+    IEnumerator Slide()
+    {
+        isSlide = true;
+        slideTimer = 0f;
+
+        while (slideTimer < slideDuration)
+        {
+            playerRigidbody.velocity = slideDirection * slideSpeed;
+
+            slideTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        playerRigidbody.velocity = Vector2.zero;
+        isSlide = false;
+
+        yield return new WaitForSeconds(slideCooldown);
+    }
+}
