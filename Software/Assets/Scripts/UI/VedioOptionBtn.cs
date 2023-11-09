7d35d8e55eea418c3d6e9f9eca3b0bffd577db56
bye-on <codnjs842@naver.com>
2023-11-09 22:11:38 +0900

    231109
    베이스캠프 맵 작업

---
+++ Software/Assets/Scripts/UI/VedioOptionBtn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VedioOptionBtn : MonoBehaviour
+{
+    public GameObject option;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void SetSolution() {
+        
+    }
+    public void OnClick() {
+        option.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
