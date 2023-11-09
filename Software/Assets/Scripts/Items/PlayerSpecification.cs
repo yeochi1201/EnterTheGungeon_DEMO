55a48337bc7e8452c9837dca06f4761a21d3fc74
박주현 <yeochi1201@gmail.com>
2023-11-09 22:16:08 +0900

    player specfication

---
+++ Software/Assets/Scripts/Items/PlayerSpecification.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpecification : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+    public float speed;
+    public float dmg;
+    public int armor;
+    public int coolness;
+    public int key;
+    public int gold;
+
+}
