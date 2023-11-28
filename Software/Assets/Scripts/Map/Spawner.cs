b8d5804697ea783bf9127466e1b5f315649cc5ea
REofCC <buzz0131@naver.com>
2023-11-28 16:16:36 +0900

    added spwanTest

--- Software/Assets/Scripts/Map/Spawner.cs
+++ Software/Assets/Scripts/Map/Spawner.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField]
-    GameObject[] enemies;
+    GameObject enemy;
     [SerializeField]
+    int spawnCount;
+
+    private CompositeCollider2D spawnRange;
 
     public void SpawmEnemies()
     {
-       
+        spawnRange = GameObject.Find("Floor").GetComponent<CompositeCollider2D>();
+        float range_X = spawnRange.bounds.size.x;
+        float range_Y = spawnRange.bounds.size.y;
+        Vector2 originPosition = GameObject.Find("Floor").transform.position;
+
+        for (int i=0;i< spawnCount;i++)
+        {
+            float randRange_X = Random.Range(1, range_X-1);
+            float randRange_Y = Random.Range(1 , range_Y-1);
+            Vector2 randomPosition = new Vector2(randRange_X, randRange_Y);
+
+            Instantiate(enemy, originPosition + randomPosition, Quaternion.identity);
+        }
     }
 }
